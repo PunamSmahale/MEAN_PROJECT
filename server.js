7fa6ead404fcfd44c057e1ddc25a21e6716d63dc
PunamSmahale <punam.mahale@gmail.com>
2015-08-15 13:58:57 -0400

    temp chages

--- server.js
+++ server.js
@@ -8,27 +8,36 @@ app.engine("html", cons.swig);
 app.set("view engine", 'html');
 app.set("views", __dirname + "/views/");
 
-var mongoConnection = mongoClient.connect(config.MONGO_URI, function(err, db){
-	if(err) throw err;
 
-console.log("Inside the Mongo connect");
-
-	app.get("/", function(request, response){
-				
-		db.collection("Names").findOne({}, function(err, doc){
-	
-			if(err) {
-				//throw err;
-				response.render("Home", err);
-			} 
-			response.render("Home", doc);
-		});	
-	});
-	
-	app.get("*", function(request, response){
+app.get("*", function(request, response){
 		response.send("Page Not Found, please try a different path.")
 	});
 	
-});
+// var mongoConnection = mongoClient.connect(config.MONGO_URI, function(err, db){
+// 	if(err) throw err;
 
-app.listen(config.PORT);
+// console.log("Inside the Mongo connect");
+
+// 	app.get("/", function(request, response){
+		
+// 			response.send("Home");
+				
+// 		// db.collection("Names").findOne({}, function(err, doc){
+	
+// 		// 	if(err) {
+// 		// 		//throw err;
+// 		// 		response.render("Home", err);
+// 		// 	} 
+// 		// 	response.render("Home", doc);
+// 		// });	
+// 	});
+	
+	
+	
+	
+	
+// });
+
+app.listen(config.PORT , function(){
+	console.log("Listening to port" + config.PORT);
+});
