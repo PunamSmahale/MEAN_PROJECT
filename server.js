7692beabb375e4fb0d6cbc0056eac96aed35e33c
PunamSmahale <punam.mahale@gmail.com>
2015-08-15 22:45:33 -0400

    change

--- server.js
+++ server.js
@@ -10,27 +10,29 @@ app.set("views", __dirname + "/views/");
 
 
 app.get("*", function(request, response){
-		response.send("Page Not Found, please try a different path.")
+	response.render("Home" , "Pratham");
+	//	response.send("Page Not Found, please try a different path.")
 	});
 	
-// var mongoConnection = mongoClient.connect(config.MONGO_URI, function(err, db){
-// 	if(err) throw err;
-
-// console.log("Inside the Mongo connect");
-
-// 	app.get("/", function(request, response){
-		
-// 			response.send("Home");
-				
-// 		// db.collection("Names").findOne({}, function(err, doc){
 	
-// 		// 	if(err) {
-// 		// 		//throw err;
-// 		// 		response.render("Home", err);
-// 		// 	} 
-// 		// 	response.render("Home", doc);
-// 		// });	
-// 	});
+var mongoConnection = mongoClient.connect(config.MONGO_URI, function(err, db){
+	if(err) throw err;
+
+console.log("Inside the Mongo connect");
+
+	app.get("/", function(request, response){
+		
+			response.send("Home");
+				
+		// db.collection("Names").findOne({}, function(err, doc){
+	
+		// 	if(err) {
+		// 		//throw err;
+		// 		response.render("Home", err);
+		// 	} 
+		// 	response.render("Home", doc);
+		// });	
+	});
 	
 	
 	
