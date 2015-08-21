baf410da19a98f87df1e898b87ea6b3781695dcb
PunamSmahale <punam.mahale@gmail.com>
2015-08-20 23:42:13 -0400

    change

--- server.js
+++ server.js
@@ -10,14 +10,13 @@ app.set("views", __dirname + "/views/");
 
 
 app.get("*", function(request, response){
-		//response.send(config.MONGO_URI + process.env.mongo_uri);
+		response.send(config.MONGO_URI + process.env.mongolab_uri);
 
-response.send("hi" + process.env.PORT);
-		mongoClient.connect(config.MONGO_URI, function(err, db){
-				if(err) response.send("Error connecting the database");
-				
-				response.send("Success!");
-		});
+		// mongoClient.connect(config.MONGO_URI, function(err, db){
+		// 		if(err) response.send("Error connecting the database");
+		// 		
+		// 		response.send("Success!");
+		// });
 	});
 	
 // var mongoConnection = mongoClient.connect(config.MONGO_URI, function(err, db){
