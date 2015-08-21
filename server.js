1ba4211974d09cdd6aa3d65d53b772fa2efae886
PunamSmahale <punam.mahale@gmail.com>
2015-08-21 00:09:49 -0400

    changes after success connection to database

--- server.js
+++ server.js
@@ -10,34 +10,23 @@ app.set("views", __dirname + "/views/");
 
 
 app.get("*", function(request, response){
+	response.send("Page Not Found");
+});
 
-		mongoClient.connect(config.MONGO_URI, function(err, db){
-				if(err) response.send("Error connecting the database");
-				
-				response.send("Success!");
-		});
+app.get("/", function(request, response){
+	mongoClient.connect(config.MONGO_URI, function(err, db){
+		if(err) throw err;
+	
+		db.collection("Names").findOne({}, function(err, doc){
+				if(err) {
+					//throw err;
+					response.render("Home", err);
+				} 
+			response.render("Home", doc);
+		});	
 	});
 	
-// var mongoConnection = mongoClient.connect(config.MONGO_URI, function(err, db){
-// 	if(err) throw err;
-
-// console.log("Inside the Mongo connect");
-
-// 	app.get("/", function(request, response){
-		
-// 			response.send("Home");
-				
-// 		// db.collection("Names").findOne({}, function(err, doc){
-	
-// 		// 	if(err) {
-// 		// 		//throw err;
-// 		// 		response.render("Home", err);
-// 		// 	} 
-// 		// 	response.render("Home", doc);
-// 		// });	
-// 	});
-	
-// });
+});
 
 app.listen(config.PORT , function(){
 	console.log("Listening to port" + config.PORT);
